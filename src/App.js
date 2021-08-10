30c0dffd3c5d1c66a3e80fe9c21e4e7dd655f37a
Sudha1919 <sudhabangaru1919@gmail.com>
2021-08-10 12:11:24 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 
+//Components
+import ClassComponent from './Components/ClassComponent';
+
  function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ClassComponent/>
     </div>
   );
 }
