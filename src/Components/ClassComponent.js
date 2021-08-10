725434546eae1139da364ed687fa1fe56bf5dedf
Sudha1919 <sudhabangaru1919@gmail.com>
2021-08-10 12:15:36 +0530

    1 file modified

--- src/Components/ClassComponent.js
+++ src/Components/ClassComponent.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class ClassComponent extends React.Component{
-constructor(){
-  super();
+class ClassComponent extends Component {
+  constructor() {
+    super();
   }
-  render(){
-    return
-     ( <div><h1>Class Component</h1></div>)
-    
+  render() {
+    return (
+      <div>
+        <h1>Class Component</h1>
+      </div>
+    );
   }
-
 }
 export default ClassComponent;
